269a5cb975e4d590e331a2414a1b93a200406b3d
sjh9391985 <sjh9391985@gmail.com>
2023-04-07 20:31:01 +0900

    Member test code

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -4,9 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-
     MemberService memberService = new MemberServiceImpl();
-
     @Test
     void join(){
 
