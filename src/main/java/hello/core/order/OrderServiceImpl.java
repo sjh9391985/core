08e3ff079f123a287cbad6efd3cdc1e65c6bfffc
sjh9391985 <sjh9391985@gmail.com>
2023-04-10 22:46:59 +0900

    DIP 지키기 위한 방법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,14 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+    private DiscountPolicy discountPolicy; // final 은 값이 무조건 할당필요
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
