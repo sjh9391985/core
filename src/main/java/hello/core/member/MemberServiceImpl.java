7d0e5137c9d8c0f5e4ee8ca4334ea80aa3557f04
sjh9391985 <sjh9391985@gmail.com>
2023-06-07 21:00:29 +0900

    feat: "회원 서비스 구현"

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,16 +2,16 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+//    public MemberServiceImpl(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
     @Override
     public void join(Member member) {
         memberRepository.save(member);
-    }
+    } // 구현체에 있는 repository가 호출
 
     @Override
     public Member findMember(Long memberId) {
